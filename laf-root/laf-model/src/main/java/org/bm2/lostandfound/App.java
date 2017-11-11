1905fc21c4d3d5a1998976729fd7cd92044dc505
Marta <pacuszka.m@gmail.com>
2017-11-12 00:41:55 +0100

    struktura projektu - moduly w mavenie

---
+++ laf-root/laf-model/src/main/java/org/bm2/lostandfound/App.java
@@ -0,0 +1,13 @@
+package org.bm2.lostandfound;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
